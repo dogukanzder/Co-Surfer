87cdd8addf70c786ab43e8ba38ed39639f389425
dogukanzder <dogukanzder@gmail.com>
2023-07-14 15:58:03 +0300

    Add project files.

---
+++ CoSurfer/GetExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoSurfer
+{
+    public class GetExample
+    {
+        public string example;
+
+        public GetExample()
+        {
+            this.example = "Selenium Coffee Test Kit,https://seleniumbase.io/coffee/, , ,0,Add Espresso to cart;Click;XPath;/html/body/div/div[2]/ul/li[1]/div/div/div[1],Open cart;Click;ClassName;pay,Write name;Write:dogukan;Name;name,Write email;Write:dogukanzder@gmail.com;Name;email,Click submit;Click;Id;submit-payment";
+        }
+
+    }
+}
---
+++ CoSurfer/PathItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoSurfer
+{
+    public class PathItem
+    {
+        public string Description { get; set; }
+        public string Function { get; set; }
+        public string How { get; set; }
+        public string Where { get; set; }
+
+    }
+}
